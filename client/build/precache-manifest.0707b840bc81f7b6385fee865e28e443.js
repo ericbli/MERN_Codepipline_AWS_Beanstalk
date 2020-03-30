a376da9fdf851ae2b091b332511a2564d7966a3a
eric li <ericbli.se@gmail.com>
2020-03-30 12:35:10 +0200

    update

---
+++ client/build/precache-manifest.0707b840bc81f7b6385fee865e28e443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85afa6d067c29da09f0a",
+    "url": "/static/css/main.03e6581c.chunk.css"
+  },
+  {
+    "revision": "85afa6d067c29da09f0a",
+    "url": "/static/js/main.85afa6d0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a0338d5c7050891abbae",
+    "url": "/static/css/2.1271a6cf.chunk.css"
+  },
+  {
+    "revision": "a0338d5c7050891abbae",
+    "url": "/static/js/2.a0338d5c.chunk.js"
+  },
+  {
+    "revision": "32b749aeebc9f9dd791c7a99d3d276c0",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7e864c132a318049fd38813f3e93c9c.js"
+  "/precache-manifest.0707b840bc81f7b6385fee865e28e443.js"
 );
 
 workbox.clientsClaim();
